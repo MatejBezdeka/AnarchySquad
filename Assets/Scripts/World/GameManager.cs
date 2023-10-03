3bf3bb6265310df3a99835a832918368fc07f2ad
Matěj Bezděka <79998445+MatejBezdeka@users.noreply.github.com>
2023-10-03 16:41:30 +0200

    a

--- Assets/Scripts/World/GameManager.cs
+++ Assets/Scripts/World/GameManager.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    public static GameManager gameManager = gameManager;
+    public event Action<float> timeChanged; 
     // Start is called before the first frame update
     void Start()
     {
