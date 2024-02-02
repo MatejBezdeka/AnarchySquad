51dfbf33b1f0c360584c5181e03df81e1aa4b439
Matěj Bezděka <matabezdeka@gmail.com>
2024-02-02 17:24:56 +0100

    a

--- Assets/Scripts/Units/EnemyUnit.cs
+++ Assets/Scripts/Units/EnemyUnit.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemyUnit : Unit {
     private SquadUnit closestEnemy = null;
     private float closestDistance = float.MaxValue;
-    // Start is called before the first frame update
     void Start() {
         base.Start();
 //         StartCoroutine(SlowUpdate());
